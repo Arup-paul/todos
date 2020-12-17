3f74341648653411d5c8dc6c39bb22a5cd162c0f
arup-paul <aruppaulctg@gmail.com>
2020-12-17 11:57:14 -0800

    some change

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,8 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
-
 export default router;
